7badba6d183af0b47cbd99925f17bc7a94b7c1c4
ivanovich18 <ivan.suralta@ctu.edu.ph>
2024-06-07 15:07:49 +0800

    Added the SplashActivity file and added a 5-second delay splash screen

---
+++ app/src/main/java/com/bscpe3g/sugboeats/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.bscpe3g.sugboeats;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 4000; // 4 seconds
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class); // Replace HomeActivity with your actual main activity
+            startActivity(mainIntent);
+            finish();
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
