dfd025d2a4787793e6f122a05f9700e0ecebf5c7
ivanovich18 <ivan.suralta@ctu.edu.ph>
2024-06-07 17:51:38 +0800

    Added Resto1 activity to the project

---
+++ app/src/main/java/com/bscpe3g/sugboeats/Resto1Activity.java
@@ -0,0 +1,21 @@
+package com.bscpe3g.sugboeats;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
+
+public class Resto1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resto1);
+    }
+}
