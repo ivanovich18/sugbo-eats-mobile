efec7215810d078daab37cfe9503b74806728f3a
kevingwapo1 <133978265+kevingwapo1@users.noreply.github.com>
2024-06-10 17:07:58 +0800

    final push

--- app/src/main/java/com/bscpe3g/sugboeats/settings.java
+++ app/src/main/java/com/bscpe3g/sugboeats/settings.java
@@ -1,24 +1,34 @@
 package com.bscpe3g.sugboeats;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class settings extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        imageView = findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to account_page
+                Intent intent = new Intent(settings.this, account_page.class);
+                startActivity(intent);
+            }
         });
+    }
+
+    public void goBack(View view) {
+        // Handle the go back logic if any
+        finish(); // or whatever logic you have for going back
     }
 }
